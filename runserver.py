37bd15e5ee6eb65104a86e94e98787c041eccf1b
louisgreenhall <62178792+louisgreenhall@users.noreply.github.com>
2020-10-17 17:29:00 +0100

    Initial commit
    First commit, template to work from in place

---
+++ runserver.py
@@ -0,0 +1,14 @@
+"""
+This script runs the MS3_Cookbook application using a development server.
+"""
+
+from os import environ
+from MS3_Cookbook import app
+
+if __name__ == '__main__':
+    HOST = environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
