992f6a544fa4a585613d3ff72ce4be852a070ca6
louisgreenhall <62178792+louisgreenhall@users.noreply.github.com>
2020-10-19 21:45:26 +0100

    Added template Carousel, Rearranged Init.js to allow call before rest of doc is loaded
    Template carousel added with same image for testing and template purposes. Errors with carousel working have been fixed by moving init.js to head (temporary measure)
    removed jQuery and replaced code with equivalent JavaScript

--- MS3_Cookbook/static/Materialize/js/init.js
+++ MS3_Cookbook/static/Materialize/js/init.js
@@ -1,7 +1,11 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelector('.carousel');
+      var instance = M.Carousel.init(elems, {
+        dist: 0,
+        padding: 0,
+        fullWidth: true,
+        indicators: true,
+        duration: 100,
+      });
+      var desktopCarousel = instance;
+});
