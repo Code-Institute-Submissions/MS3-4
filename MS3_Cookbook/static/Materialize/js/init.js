ca0c0cb1d5ed710332f0f329c6f2ba8bb0937996
louisgreenhall <62178792+louisgreenhall@users.noreply.github.com>
2020-10-19 21:47:07 +0100

    Added button funcionality to carousel
    Floating buttons now provide left and right momoevment between carousel items

--- MS3_Cookbook/static/Materialize/js/init.js
+++ MS3_Cookbook/static/Materialize/js/init.js
@@ -8,4 +8,9 @@
         duration: 100,
       });
       var desktopCarousel = instance;
+
+const FBR = document.getElementById("btn-right");
+FBR.addEventListener("click", function(){ instance.next()});
+const FBL = document.getElementById("btn-left");
+FBL.addEventListener("click", function(){ instance.prev()});
 });
