287da79d9ef41c663c9423168c479500be070137
louisgreenhall <62178792+louisgreenhall@users.noreply.github.com>
2020-10-19 21:47:55 +0100

    Removed caching from html frontend

--- MS3_Cookbook/views.py
+++ MS3_Cookbook/views.py
@@ -16,7 +16,17 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 
-
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 @app.route('/')
 @app.route('/home')
@@ -55,6 +65,7 @@ def auth():
 
     cursor.execute("SELECT * from recipe")
     data = cursor.fetchone()
+  
  
     password = "hello"
     return jsonify(password=password)
