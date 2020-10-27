336f8a16b3d3ad19821909fe1f969fb5dbe72601
louisgreenhall <62178792+louisgreenhall@users.noreply.github.com>
2020-10-27 18:27:17 +0000

    Updated views.py
    Added random recipe page, to be linked to a button which will take users to a randomly selected recipe from collection

--- MS3_Cookbook/views.py
+++ MS3_Cookbook/views.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template, jsonify
 from flask_mongoengine import MongoEngine
 from flask_pymongo import PyMongo
 from MS3_Cookbook import app
-
+from random import uniform
 
 mongo = PyMongo(app)
 
@@ -30,7 +30,7 @@ def add_header(r):
 def home():
     """Renders the home page."""
 
-    # recipe = list(mongo.db.recipe.find())
+    recipe1 = mongo.db.recipe.find_one({"_id":1})
     recipe = mongo.db.recipe.find_one({"_id": 26})
     
     recipes1 = mongo.db.recipe.find()[1:5]
@@ -64,21 +64,31 @@ def about():
         year=datetime.now().year,
         message='Your application description page.')
 
-@app.route('/api/recipe/<recipe_id>')
+@app.route('/recipe/<recipe_id>')
 def recipe(recipe_id):
     recipe = mongo.db.recipe.find_one({"_id":int(recipe_id)})
     
-    return jsonify(recipe)
+    return render_template(
+        'recipe.html',
+        title=recipe['Name'],
+        recipe=recipe)
+
+@app.route('/recipe/random')
+def random_recipe():
+    Doc_total = mongo.db.recipe.count()
+    random = uniform(1, Doc_total)
+    recipe = mongo.db.recipe.find_one({"_id":int(random)})
+    
+    return render_template(
+        'recipe.html',
+        title=recipe['Name'],
+        recipe=recipe)
+
 
 @app.route('/api/recipe/<recipe_id>/categories')
 def recipe_categories(recipe_id):
     recipe = mongo.db.recipe.find_one({"_id":int(recipe_id)}, projection={"Categories": True})
     
     return jsonify(recipe)
- 
-
-
-
-
 
 ### mongo "mongodb+srv://cookbook.3ljpp.mongodb.net/Cookbook" --username Lou
